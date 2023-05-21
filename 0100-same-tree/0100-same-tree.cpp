0c3664853d45e4122ed8b40d30c9aa4d0da89cec
Ivan Lam <44384981+lamdaham@users.noreply.github.com>
2023-05-21 11:15:54 -0400

    Time: 0 ms (100.00%), Space: 9.9 MB (94.19%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL || q == NULL) {
+            return q == NULL && p == NULL;
+        }
+        if(p->left == NULL && p->right == NULL) {
+            return p->val == q->val && q->left == NULL && q->right == NULL;
+        }
+        if(p->right != NULL && q->right != NULL && p->left != NULL && q->left != NULL) {
+            return (p->val == q->val && isSameTree(p->right, q->right)  && isSameTree(p->left, q->left));
+        }
+        if(p->right != NULL && q->right != NULL) {
+            return (p->val == q->val  && isSameTree(p->right, q->right) && p->left == NULL && q->left == NULL);
+        }
+        if(p->left != NULL && q->left != NULL) {
+            return (p->val == q->val  && isSameTree(p->left, q->left) && p->right == NULL && q->right == NULL);
+        }
+        return false;
+        
+    }
+};
